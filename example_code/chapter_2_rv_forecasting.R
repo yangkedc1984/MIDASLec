cbd68764dabe0369b494c7aa092431aaea7b5e69
Jonas Striauias <jonas.striaukas@gmail.com>
2019-08-08 13:14:28 +0200

    v0.1
    update:example2

--- example_code/chapter_2_rv_forecasting.R
+++ example_code/chapter_2_rv_forecasting.R
@@ -7,3 +7,22 @@ require("MIDASLec")
 
 # --- load data --- #
 data("example2")
+
+# --- options --- #
+optionsmidas <- list(aggrX =  126) # no. of lags of RV
+#Horizons = c(5, 10, 22, 44, 66) # forecasting horizons
+Horizons = c(5, 10) # forecasting horizons
+
+
+# --- main loop through assets --- #
+
+for (countf in 1:5) {
+  Rd <- AllclosingRet.2018[[countf]]
+  dates <- Alldates.2018[[countf]]
+  RV <- AllRV_2018{countf,1}
+  t <- size(dates,1) # no of time-series obs
+  
+  
+  
+  
+} # end of loop through assets
